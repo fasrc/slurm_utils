60dc16500be1db2e5d7089d1faeb2591f8d3b898
John Brunelle <john_brunelle@harvard.edu>
2014-03-04 14:02:12 -0500

    add a job dependency example

---
+++ example_workflows/job_dependencies.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# The sbatch option `--dependency' can be used to coordinate when jobs run.  
+# This script shows a simple example of requiring two jobs to run in 
+# non-overlapping, sequential order.
+
+# stop if any commands fail
+set -e 
+
+# submit first job and record its job id
+jobid1=$(sbatch -J job1 -o job1.out --wrap date | awk '{print $NF}')
+echo "job1 is: $jobid1"
+
+jobid2=$(sbatch -J job2 -o job2.out --dependency=afterok:"$jobid1" --wrap date | awk '{print $NF}')
+echo "job2 is: $jobid2"
+
+echo "job2 ($jobid2) should not start until job1 ($jobid1) finishes successfully"
