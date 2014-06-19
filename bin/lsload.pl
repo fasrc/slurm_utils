c392c6dd7aac7c68c212d943e9a746fe03907931
Aaron Kitzmiller <aaronkitzmiller@RC-AKITZMILLER-1.local>
2014-06-19 13:32:26 -0400

    Use field names instead of field order

--- bin/lsload.pl
+++ bin/lsload.pl
@@ -1,19 +1,19 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 @lines = qx/scontrol -o show node/;
 printf ("%-14s %6s %6s %6s %6s %6s %6s   %-15s\n","Hostname","Cores","InUse","Ratio","Load","Mem","Alloc","State");
 foreach (@lines){
-  @e=split(" ",$_); @r=split("=",$_); $reason=pop(@r);
-  chomp($reason);
-  ($c,$cv)=split("=",$e[5]); ($cu,$cuv)=split("=",$e[3]);
-  ($mu,$muv)=split("=",$e[13]); ($m,$mv)=split("=",$e[12]);
-  ($n,$nv)=split("=",$e[0]); ($l,$lv)=split("=",$e[6]);
-  ($s,$sv)=split("=",$e[16]); if ($e[1]=~/Arch/){
-    printf ("%-14s %6d %6d",$nv,$cv,$cuv);
-    if($cuv>0){ printf (" %6.1f ",($cuv/$cv)*100); }
-    else { printf (" %6.1f ", "0.0"); }
-    printf ("%6.2f %6d %6d   %-15s ",$lv,int($mv/1000),int($muv/1000),$sv);
-    if ($reason!~ /n\/s/){print "$reason\n";}
-    else {print "\n";}
-  }
-  else {printf ("%-14s %6s %6s %6s %6s %6s %6s   %-15s %s\n",$nv."(d)",0,0,0.0,0.00,0,0,"DOWN",$reason);}
+    chomp;
+    ($reason) = $_ =~ /Reason=(.*)/;
+    %data = map {/([^= ]+)/ => /=\s*(.*)/} split /\s+/;
+    $data{'Reason'} = $reason;
+   if ($data{'Arch'}){
+        printf ("%-14s %6d %6d",$data{'NodeName'},$data{'CPUTot'},$data{'CPUAlloc'});
+        if ($data{'CPUAlloc'} > 0){ printf (" %6.1f ",( $data{'CPUAlloc'} / $data{'CPUTot'} ) * 100); }
+        else { printf (" %6.1f ", "0.0"); }
+        printf ("%6.2f %6d %6d   %-15s ",$data{'CPULoad'},int($data{'RealMemory'} / 1000),int( $data{'AllocMem'} / 1000 ),$data{'State'});
+        if ($data{'Reason'}) {print $data{'Reason'};}
+        print "\n";
+   }
+   else {printf ("%-14s %6s %6s %6s %6s %6s %6s   %-15s %s\n",$data{'NodeName'}."(d)",0,0,0.0,0.00,0,0,"DOWN",$data{'Reason'});}
 }
+
